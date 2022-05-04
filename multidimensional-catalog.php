69d8ef4fbccf14a378df8957d31cc52aa5ca78cf
Emilien MACE <emilien.mace@le-campus-numerique.fr>
2022-05-04 16:02:16 +0200

    fin étape 4, boucles, foreach associtée Ãaux produits

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -1,37 +1,56 @@
 <?php
 
-$products = [
-
-$chaise = [
-    "name" => "chaise",
-    "price" => 50,
-    "weight" => 7,
-    "discount" => 10,
-    "picture_url" => "https://static.westwingnow.de/image/upload/w_1500,h_2000,pd_272_120_272_120,c_limit/simple/24/1661/1405672.jpg"
-],
-
-$oranges = [
-    "name" => "oranges",
-    "price" => 3,
-    "weight" => 1,
-    "discount" => null,
-    "picture_url" => "https://produits.bienmanger.com/34499-0w0h0_Oranges_Vanille_Sicile_Bio.jpg"
-],
-
-
-$beezwrap = [
-    "name" => "beezwrap",
-    "price" => 8,
-    "weight" => 0.1,
-    "discount" => null,
-    "picture_url" => "https://c-pi.niceshops.com/upload/image/product/medium/default/bees-wrap-vegan-lot-de-7-1-kits-813914-fr.jpg"
-],
-];
-
-
 require "header.php";
 
+$products = [
+
+    $chaise = [
+        "name" => "chaise",
+        "price" => 50,
+        "weight" => 7,
+        "discount" => 10,
+        "picture_url" => "https://static.westwingnow.de/image/upload/w_1500,h_2000,pd_272_120_272_120,c_limit/simple/24/1661/1405672.jpg"
+    ],
+
+    $oranges = [
+        "name" => "oranges",
+        "price" => 3,
+        "weight" => 1,
+        "discount" => null,
+        "picture_url" => "https://produits.bienmanger.com/34499-0w0h0_Oranges_Vanille_Sicile_Bio.jpg"
+    ],
+
+
+    $beezwrap = [
+        "name" => "beezwrap",
+        "price" => 8,
+        "weight" => 0.1,
+        "discount" => null,
+        "picture_url" => "https://c-pi.niceshops.com/upload/image/product/medium/default/bees-wrap-vegan-lot-de-7-1-kits-813914-fr.jpg"
+    ],
+];
+
+echo '<div class="row row-cols-1 row-cols-md-3 g-4 mb-5" id="products">';
+
+foreach ($products as $key => $value) {
+
+    echo '<div class="col">';
+    echo '<a href="page_produit.html">';
+    echo '<div class="card text-center">';
+    echo '<img src="' . $value ["picture_url"] . '"class="card-img-top" alt="...">';
+    echo '<div class="card-body">';
+    echo '<h5 class="card-title">' . $value["name"] . '</h5>';
+    echo '<p>Prix : ' . $value["price"] . '</p>';
+    echo '<p>Remise : ' . $value["discount"] . '</p>';
+    echo '<p>Poids : ' . $value["weight"] . '</p>';
+    echo '</div></div></a></div>';
+
+}
+echo '</div>';
+
+
 ?>
+
 
     <div class="row row-cols-1 row-cols-md-3 g-4 mb-5" id="products">
         <div class="col">
