cd7f31bcffc11b9b57527ffd625bd99be3d88089
bobacar.boutayeb <bobacar.boutayeb@le-campus-numerique.fr>
2022-05-31 14:12:37 +0200

    cleaning

--- connect.php
+++ connect.php
@@ -3,9 +3,9 @@
 try
 {
     $db = new PDO(
-        'mysql:host=localhost;dbname=bdd_boutique_amazen;charset=utf8',
-        'emilienm',
-        'akasha38!',
+        'mysql:host=localhost;dbname=amazen;charset=utf8',
+        'bobacar',
+        'jenesaispas!',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 }
@@ -13,11 +13,3 @@ catch (Exception $e)
 {
     die('Erreur : ' . $e->getMessage());
 }
- /*
-$amazenStatement = $db->prepare('SELECT * FROM categories ');
-$amazenStatement->execute();
-$categories = $amazenStatement->fetchAll();
-echo '<pre>';
-print_r($categories);
-echo '</pre>';
-*/
